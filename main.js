f2dd09f3083bf6d8d52abf9140189a434601556e
Anja Weigelt <aweigelt@ufl.edu>
2019-10-20 13:05:14 -0400

    Add Hide/Expand Description Functionality

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
-// Access Accordion Headers
+// Variables
 var accordionHeaders = document.querySelectorAll('#accordion .accordion-headers');
+var accordionDescriptions = document.querySelectorAll ('#accordion .accordion-description');
 
 // Accordion Dropdown Action
 function expandAccordionDescription() {
 
-  // Toggle Indicators
   for (i = 0; i < accordionHeaders.length; i++) {
+    //Toggle Indicators
     accordionHeaders[i].childNodes[3].innerHTML = '+';
+    //Hide Accordion Descriptions
+    accordionDescriptions[i].classList.remove('accordion-open');
   }
+
+  //Make Hidden Description Appear
+  this.nextElementSibling.classList.add('accordion-open');
+
+  //Switch from Plus Sign to Minus Sign when Toggled
   this.childNodes[3].innerHTML = '-';
+
 }
 
 // Add Click Event Listener for Accordion Headers
