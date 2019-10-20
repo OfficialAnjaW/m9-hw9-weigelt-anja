4213310c7fdd01b5eb67526eab07710417901526
Anja Weigelt <aweigelt@ufl.edu>
2019-10-20 12:33:08 -0400

    Set up Accordion Dropdown Action

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("hello");
+// Access Accordion Headers
+var accordionHeaders = document.querySelectorAll('#accordion .accordion-headers');
+
+// Accordion Dropdown Action
+function expandAccordionDescription() {
+
+  // Toggle Indicators
+  for (i = 0; i < accordionHeaders.length; i++) {
+    accordionHeaders[i].childNodes[3].innerHTML = '+';
+  }
+  this.childNodes[3].innerHTML = '-';
+}
+
+// Add Click Event Listener for Accordion Headers
+for (i = 0; i < accordionHeaders.length; i++) {
+  accordionHeaders[i].addEventListener('click', expandAccordionDescription);
+}
